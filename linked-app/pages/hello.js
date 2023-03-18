a35f36bbe160e04b6448919d18b060977d09bede
Donhuey <35469316+donhuey@users.noreply.github.com>
2023-03-18 06:03:45 -0500

    rando chicago

---
+++ linked-app/pages/hello.js
@@ -0,0 +1,14 @@
+export default function hello(){
+    return (
+    
+    <>
+    <h1>Hello yall</h1>
+
+    <p>click the button below to hide/show this text</p>
+    
+    <button> HIDE or SHOW</button>
+
+    </>
+
+    );
+}
