590c2f9ff6ddb856aabb4f7eaff6cdb5a8cacbcf
Donhuey <35469316+donhuey@users.noreply.github.com>
2023-03-20 22:50:48 -0500

    pushing tut changes

---
+++ linked-app/.next/server/pages/hello.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/hello";
+exports.ids = ["pages/hello"];
+exports.modules = {
+
+/***/ "./pages/hello.js":
+/*!************************!*\
+  !*** ./pages/hello.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ hello)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction hello() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"Hello yall\"\n            }, void 0, false, {\n                fileName: \"/Users/donhuey/Desktop/Coding/nextLinked/learning-nextjs-2491193/linked-app/pages/hello.js\",\n                lineNumber: 5,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n                children: \"click the button below to hide/show this text\"\n            }, void 0, false, {\n                fileName: \"/Users/donhuey/Desktop/Coding/nextLinked/learning-nextjs-2491193/linked-app/pages/hello.js\",\n                lineNumber: 7,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"button\", {\n                children: \" HIDE or SHOW\"\n            }, void 0, false, {\n                fileName: \"/Users/donhuey/Desktop/Coding/nextLinked/learning-nextjs-2491193/linked-app/pages/hello.js\",\n                lineNumber: 9,\n                columnNumber: 5\n            }, this)\n        ]\n    }, void 0, true);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9oZWxsby5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQWUsU0FBU0EsUUFBTztJQUMzQixxQkFFQTs7MEJBQ0EsOERBQUNDOzBCQUFHOzs7Ozs7MEJBRUosOERBQUNDOzBCQUFFOzs7Ozs7MEJBRUgsOERBQUNDOzBCQUFPOzs7Ozs7OztBQUtaLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9saW5rZWQtYXBwLy4vcGFnZXMvaGVsbG8uanM/Y2I0ZSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBoZWxsbygpe1xuICAgIHJldHVybiAoXG4gICAgXG4gICAgPD5cbiAgICA8aDE+SGVsbG8geWFsbDwvaDE+XG5cbiAgICA8cD5jbGljayB0aGUgYnV0dG9uIGJlbG93IHRvIGhpZGUvc2hvdyB0aGlzIHRleHQ8L3A+XG4gICAgXG4gICAgPGJ1dHRvbj4gSElERSBvciBTSE9XPC9idXR0b24+XG5cbiAgICA8Lz5cblxuICAgICk7XG59XG4iXSwibmFtZXMiOlsiaGVsbG8iLCJoMSIsInAiLCJidXR0b24iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/hello.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/hello.js"));
+module.exports = __webpack_exports__;
+
+})();
